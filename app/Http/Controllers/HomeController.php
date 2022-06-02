71bb2582f1a83521fc6b11833be1d314cf8a44b5
herndev <herniejabien45@gmail.com>
2022-06-02 15:48:13 +0800

    Updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 Use \Carbon\Carbon;
 use App\Models\Agenda;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -26,14 +27,14 @@ class HomeController extends Controller
     public function index()
     {
         $mytime = Carbon::now();
-        $agendas = Agenda::all();
+        $agendas = Agenda::where('user_id', Auth::user()->id)->get();
         return view('home', ['time' => $mytime->format('d-m-Y'), "agendas" => $agendas]);
     }
 
     public function edit($id)
     {
         $mytime = Carbon::now();
-        $agendas = Agenda::all();
+        $agendas = Agenda::where('user_id', Auth::user()->id)->get();
         $agenda = Agenda::find($id);
         return view('home', ['time' => $mytime->format('d-m-Y'), "agendas" => $agendas, "id"=>$id, "editagenda"=> $agenda]);
         // return dd($id);
