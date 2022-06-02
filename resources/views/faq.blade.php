ebc8cbaa6977ceba68e4071ceb78825717aadb21
herndev <herniejabien45@gmail.com>
2022-06-02 11:01:20 +0800

    Updated

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -8,7 +8,7 @@
     <div class="container" id="mywelcome">
         <div class="row justify-content-center">
             <div class="col">
-                <img src="img/motivational.png" alt="" class="img-fluid w-100">
+                <img src="img/faq.png" alt="" class="img-fluid w-100">
             </div>
         </div>
     </div>
