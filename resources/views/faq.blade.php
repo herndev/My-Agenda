4236ab6a51d7e6d1f8a1ef019094bedb8fe78e28
herndev <herniejabien45@gmail.com>
2022-06-02 10:59:57 +0800

    Updated

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.motivation')
+
+@section('header')
+Motivation Wall    
+@endsection
+
+@section('content')
+    <div class="container" id="mywelcome">
+        <div class="row justify-content-center">
+            <div class="col">
+                <img src="img/motivational.png" alt="" class="img-fluid w-100">
+            </div>
+        </div>
+    </div>
+@endsection
